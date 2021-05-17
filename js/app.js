cd757ce76955098f0d10dc06111f79a5c2930f89
Romuald01 <romualdadjimagbe2018@gmail.com>
2021-05-17 23:12:07 +0100

    progress crowdfunding

--- js/app.js
+++ js/app.js
@@ -3,48 +3,60 @@ var changeText = document.getElementById("icon-bookmarked-text");
 var bookMarkedLogo;
 
 bookMarkedIcon.addEventListener("click",function(){ 
-  changeText.style.color = "hsl(176, 72%, 28%)";
+changeText.style.color = "hsl(176, 72%, 28%)";
 changeText.innerHTML = "<b>bookmarked</b>";
 var bookMarkedLogo = bookMarkedIcon.setAttribute("src","images/icon-bookmarked.svg");
 bookMarkedLogo.style.display = "block";
   });
-    
-  // var bookMarkedLog =
-  // bookMarkedLogo.addEventListener("click",function(){ 
-  //   changeText.style.color = "red";
-  // changeText.innerHTML = "<b>bookmark</b>";
-  // bookMarkedIcon = bookMarkedIcon.setAttribute("src","images/icon-bookmark.svg");
-  // bookMarkedIcon.style.display = "block";
-  //   });
+  // document.querySelector('#show-modal').addEventListener('click',()=> {
+  //   alert("hello world!");
+  // })
+
+  // CODE FOR THANKS SUPPORT START
+const toggleModalThanks = ()=>{
+  document.querySelector('.modal')
+  .classList.toggle('.modal-hidden');
+
+};
 
 
-function support(){
-  var thanksBox =  document.getElementById("thankSupportContent");
-  thanksBox.style.display = "block";
-}
+document.querySelector('#show-modal')
+.addEventListener('click',toggleModalThanks);
+// document.querySelector('#show-modal')
+// .addEventListener('submit',(event)=>{
+// event.preventDefault();
+// toggleModalThanks();
+// });
 
-// code of the navbar overlay start
+document.querySelector('#modal_close')
+.addEventListener('click',toggleModalThanks);
+
+  // CODE FOR THANKS SUPPORT END
+// function support(){
+//   var thanksBox =  document.getElementById("thankSupportContent");
+//   thanksBox.style.display = "block";
+// }
+
+
+
+// mobile nav overlay start
 document.querySelector("#close-icon").style.display = "none";
-
-var $hamburgerIcon = document.querySelector("#hamburger-icon");
-var $closeIcon = document.querySelector("#close-icon");
+var  $hamburgerIcon = document.getElementById("hamburger-icon");
+var  $closeIcon = document.getElementById("close-icon");
 
 $hamburgerIcon.addEventListener("click",()=>{
-document.querySelector("#myNav").style.width = "100%";
-
-$hamburgerIcon.style.display = "none";
-$closeIcon.style.display = "block";
-
-});
-
-$closeIcon.addEventListener("click",()=>{
-document.querySelector("#myNav").style.width = "0%";
-$hamburgerIcon.style.display = "block";
-$closeIcon.style.display = "none";
-})
-// code of the navbar overlay end
-
-
+  document.getElementById("myNav").style.width = "100%";
+  document.body.classList.add('no-scroll');
+  
+  $hamburgerIcon.style.display = 'none';
+  $closeIcon.style.display = 'block';
  
+})
 
-                                                                                                                                                                                                                                                                                                                  
+$closeIcon.addEventListener('click',()=>{
+ document.getElementById("myNav").style.width = "0%";
+ document.body.classList.remove('no-scroll');
+ $hamburgerIcon.style.display = 'block';
+ $closeIcon.style.display = 'none';
+})
+// mobile nav overlay start
